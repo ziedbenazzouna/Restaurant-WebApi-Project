e8e654c95380bdb571f43756cdf1ae26c44e744d
Zied Ben Azzouna <zied.benazzouna@gmail.com>
2021-09-06 11:41:06 +0100

    Configure Identity Core & Create registers users service into Identity Core

---
+++ RestaurantProject/Controllers/ApplicationUserController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RestaurantProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantProject.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ApplicationUserController : ControllerBase
+    {
+        private UserManager<IdentityUser> _userManager;
+        private SignInManager<IdentityUser> _singInManager;
+
+        public ApplicationUserController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _singInManager = signInManager;
+        }
+
+        [HttpPost]
+        [Route("Register")]
+        // POST : /api/ApplicationUser/Register
+        public async Task<Object> PostApplicationUser(ApplicationUserModel model)
+        {
+            var applicationUser = new IdentityUser()
+            {
+                UserName = model.UserName,
+                Email = model.Email,
+            };
+
+            try
+            {
+                var result = await _userManager.CreateAsync(applicationUser, model.Password);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
--- RestaurantProject/Startup.cs
+++ RestaurantProject/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -29,9 +30,21 @@ namespace RestaurantProject
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-             // options.UseSqlServer(
-             //   Configuration.GetConnectionString("DefaultConnection")));
-              options.UseSqlite("Data Source = RestauXpressDB.db"));
+              options.UseSqlServer(
+                Configuration.GetConnectionString("DefaultConnection")));
+            //  options.UseSqlite("Data Source = RestauXpressDB.db"));
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 4;
+            }
+            );
             services.AddControllers();
             services.AddTransient(typeof(IRepository<>), typeof(Repository<>));
             services.AddTransient<IProductService, ProductService>();
